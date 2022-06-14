d4b0b0dbfa79791e6bb728b7950546b75c6e1a47
Emil <emillindau@gmail.com>
2022-06-14 21:51:49 +0200

    Initial commit :)

---
+++ index.mjs
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+import boxen from "boxen";
+import chalk from "chalk";
+
+const boxenOptions = {
+  padding: 1,
+  width: 74,
+  title: "Mpya Digital",
+  titleAlignment: "center",
+  borderStyle: "round",
+  borderColor: "#ffc107",
+};
+
+const intro1 = chalk.bold(
+  "Hello there! We're Mpya Digital. Our mantra is Work is Joy, and it is important for us that all our employees find their work enjoyable!\n\n"
+);
+const intro2 = chalk.underline("Find us the internet:\n\n");
+const intro = intro1 + intro2;
+
+const links = [
+  {
+    name: chalk.hex("#fff").bgHex("#E34586")(" Website  "),
+    url: "   https://mpyadigital.com",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#E34586")(" Linkedin "),
+    url: "   https://www.linkedin.com/company/mpya-digital",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#E34586")(" Instagram"),
+    url: "   https://www.instagram.com/mpyadigital",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#E34586")(" Medium   "),
+    url: "   https://medium.com/@mpyadigital",
+  },
+];
+
+const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
+
+const joinUs1 = chalk.bold(
+  "\n\nWe are always on the lookout for new courageous and curious software developers.\n\n"
+);
+const joinUs2 = chalk.underline("Why don't you joyn us?\n\n");
+const joinUs = joinUs1 + joinUs2;
+
+const joinLinks = [
+  {
+    name: chalk.hex("#fff").bgHex("#00B2AA")("Email"),
+    url: "       hello@mpyadigital.com",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#00B2AA")("Phone"),
+    url: "       +46 (0)10-330 20 13",
+  },
+];
+
+const joinList = joinLinks.map(({ name, url }) => `${name} ${url}`).join("\n");
+
+console.log(boxen(intro + linkList + joinUs + joinList, boxenOptions));
